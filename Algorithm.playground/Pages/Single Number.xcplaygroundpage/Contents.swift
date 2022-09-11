7f13bf9361993b133187013d6475eee9fe499a51
JangJumyeong <isouvezz@gmail.com>
2022-09-11 23:40:13 +0900

    Single Number 풀이

---
+++ Algorithm.playground/Pages/Single Number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+func singleNumber(_ nums: [Int]) -> Int {
+    
+    /* 첫 코드 2468ms 말도안되는 값....
+    if nums.count == 1 {
+        return nums.first!
+    } else {
+        
+        var tempArray = nums
+        while true {
+            if tempArray.count == 1 {
+                return tempArray.first!
+            } else {
+                let checkItem = tempArray.first!
+                let checkArray = tempArray[1...]
+                if checkArray.contains(checkItem) {
+                    tempArray = tempArray.filter{ $0 != checkItem }
+                } else {
+                    return checkItem
+                }
+            }
+        }
+        
+    }*/
+    let tempArray = nums
+    
+    for num in nums {
+        if tempArray.filter({ $0 == num }).count == 1 {
+            return num
+        }
+    }
+    
+    return 0
+
+}
+
+let test = [4,1,2,1,2]
+
+singleNumber(test)
